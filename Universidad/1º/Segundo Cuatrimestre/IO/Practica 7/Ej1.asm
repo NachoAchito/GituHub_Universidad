81d934df9de202b4d5ea2769c45ddf6316c33ca7
NachoAchito <nachonachora@gmail.com>
2022-06-16 10:59:28 +0200

    .

--- Universidad/1º/Segundo Cuatrimestre/IO/Practica 7/Ej1.asm
+++ Universidad/1º/Segundo Cuatrimestre/IO/Practica 7/Ej1.asm
@@ -1,47 +1,83 @@
-.org 100h
+.org 50h
 	pila:
-
-.org 200h
+.org 100h
 	lxi H, pila
-	sphl
+	sphl 
 
-	lxi B, E000h
-	lxi H, 400h
-	mvi D, 02h
+	LXI H, E000h
+	JMP loop
 
-bucle:
-	jmp bucle
-
-.org 0024h
-	call string_in
- 	ret
+.org 600h
+	loop:
+	JMP loop
 
 
-.org 300h
-string_in:
-	in 00h
-	cpi 00h
-	jz no_tecla
+.org 24h
+	IN 00h
+	MOV M, A
+	INX H
+	CPI 3Dh
+	JZ guardar
+	JMP loop
+	JMP loop
+
+;Subrutina que guarda el valors de memoria als registres i escull l'operacio que es
+;realitzara amb els valors dels registres
+.org 200h
+guardar:
+	MOV B, H
+	MOV C, L
+
+	DCX B
+	DCX B
+
+	LDAX B
+	SUI 30h
+	MOV E, A
+
+	DCX B
+	DCX B
+	LDAX B
+	SUI 30h
+	MOV D, A
+	INX B
+
+	LDAX B
+	CPI 2Bh
+	JZ suma
+
+;Subrutina que realitza la suma
+.org 250h
+suma:
+	MOV A, D
+	ADD E
+
+	SUI Ah
+	JP carry
+
+	ADI 3Ah
+	MOV M, A
+	INX H
 	
-	mov M, A
-	inx H
-	dcr D
-	mvi A, 0h
-	cmp D
-	jz resultat
+	MVI A, 3Bh
+	MOV M, A
+	INX H
+	
+	JMP loop
 
-no_tecla:
-	ret
+;Subrutina que gestiona el resultat si hi ha un carry en la suma
+.org 300h
+carry:
+	MVI M, 31h
+	INX H
+	ADI 30h
+	MOV M, A
+	INX H
 
-resultat:
-	mov A, H
-	dcr H
-	add H
-	sui 30h
-	stax B
-	inx B
-	mvi D, 2h
-	ret
+	MVI A, 3Bh
+	MOV M, A
+	INX H
 
+	JMP loop
 
 
